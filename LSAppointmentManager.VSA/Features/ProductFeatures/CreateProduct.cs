09ad2936d0a0049edefb83cd03c9f8ecaa54584c
Roberto Aguilar <justtorales@gmail.com>
2024-10-10 01:46:33 -0600

    Added basic product services in 'Features' folder

---
+++ LSAppointmentManager.VSA/Features/ProductFeatures/CreateProduct.cs
@@ -0,0 +1,13 @@
+﻿using LSAppointmentManager.Entities;
+using LSAppointmentManager.Repositories.Repositories;
+using LSAppointmentManager.VSA.Features.AbstractFeatures;
+using static LSAppointmentManager.VSA.Profiles.ProductProfiles;
+
+
+namespace LSAppointmentManager.VSA.Features.ProductFeatures
+{
+    public class CreateProduct : AbstractCreateFeature<ProductRepository, Product, CreateProductDto.Request, CreateProductDto.Response>
+    {
+
+    }
+}
---
+++ LSAppointmentManager.VSA/Features/ProductFeatures/DeleteProduct.cs
@@ -0,0 +1,11 @@
+﻿using LSAppointmentManager.Entities;
+using LSAppointmentManager.Repositories.Repositories;
+using LSAppointmentManager.VSA.Features.AbstractFeatures;
+
+
+namespace LSAppointmentManager.VSA.Features.ProductFeatures
+{
+    public class DeleteProduct : AbstractDeleteFeature<ProductRepository, Product>
+    {
+    }
+}
