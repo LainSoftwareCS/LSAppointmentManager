a7ae5b89737efa2c983df56a34adcd061343f1c3
Roberto Aguilar <justtorales@gmail.com>
2024-10-10 01:49:28 -0600

    Create UpdateProduct.cs in 'Features' folder

---
+++ LSAppointmentManager.VSA/Features/ProductFeatures/UpdateProduct.cs
@@ -0,0 +1,11 @@
+﻿using LSAppointmentManager.Entities;
+using LSAppointmentManager.Repositories.Repositories;
+using LSAppointmentManager.VSA.Features.AbstractFeatures;
+using static LSAppointmentManager.VSA.Profiles.ProductProfiles;
+
+namespace LSAppointmentManager.VSA.Features.ProductFeatures
+{
+    public class UpdateProduct : AbstractUpdateFeature<ProductRepository, Product, UpdateProductDto.Request, UpdateProductDto.Response>
+    {
+    }
+}
