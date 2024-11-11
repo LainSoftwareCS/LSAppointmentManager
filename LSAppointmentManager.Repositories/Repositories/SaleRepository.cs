8f369381d9c79b377bc75ae1deab791d1fdd12eb
Roberto Aguilar <136756233+RobertoAguilarT@users.noreply.github.com>
2024-11-10 23:14:40 -0600

    Terminacióndel Feature CreateSale + métodos en Repositories correspondientes

--- LSAppointmentManager.Repositories/Repositories/SaleRepository.cs
+++ LSAppointmentManager.Repositories/Repositories/SaleRepository.cs
@@ -1,12 +1,24 @@
 ﻿using LSAppointmentManager.Entities;
 using LSAppointmentManager.Repositories.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
-namespace LSAppointmentManager.Repositories.Repositories
+namespace LSAppointmentManager.Repositories.Repositories;
+
+public class SaleRepository : AbstractRepository<Sale>
 {
-    public class SaleRepository : AbstractRepository<Sale>
+    public SaleRepository(LSAppointmentManagerContext context) : base(context)
     {
-        public SaleRepository(LSAppointmentManagerContext context) : base(context)
-        {
-        }
+        
+        
+    }
+    public async Task CreateAsync(Sale sale)
+    {
+        await context.Sales.AddAsync(sale);
+        await context.SaveChangesAsync();
+    }
+    public async Task<Customer> GetCustomerByIdAsync(int customerId)
+    {
+        return await context.Customers.FirstOrDefaultAsync(c => c.Id == customerId);
     }
 }
